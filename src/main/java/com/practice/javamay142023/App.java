9b598a80b093163cce6e3a25cc165a493c02ab8f
Bijen Adhikari <99107004+Bijen-Adhikari@users.noreply.github.com>
2023-05-14 16:35:05 -0400

    initialization with my file

---
+++ src/main/java/com/practice/javamay142023/App.java
@@ -0,0 +1,23 @@
+package com.practice.javamay142023;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    Scanner sc=new Scanner(System.in);
+	System.out.println("Enter any number");
+	int num=sc.nextInt();
+	if(num%2==0) {
+		System.out.println("even");}
+	
+	else {
+		System.out.println("odd");
+		}
+	}
+}
+	
---
+++ src/main/java/com/practice/javamay142023/may142023.java
@@ -0,0 +1,21 @@
+package com.practice.javamay142023;
+import java.util.Scanner;
+public class may142023 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter any number");
+		int num=sc.nextInt();
+		if(num%2==0) {
+			System.out.println("even");}
+		
+		else {
+			System.out.println("odd");
+			}
+		}
+		}
+		
+		
+	
+
+}
